6e1228af968bb4eb92c05b4242d670a975acf332
idkwtdoob <rkdwndms333@naver.com>
2023-07-08 11:33:29 +0900

    수정 완료

---
+++ FrontEnd/assignment/week02/TodoList/js/todo.js
@@ -0,0 +1,29 @@
+const toDoFrom=document.getElementById("todo-form");
+const toDoInput=document.querySelector("#todo-form input");
+const toDoList=document.getElementById("todo-list");
+
+function paintToDo(newTodo) {
+    const li = document.createElement("li");
+    const span=document.createElement("span");
+    li.appendChild(span);
+    span.innerHTML=newTodo;
+    const button=document.createElement("button");
+    button.innerText="✅";
+    button.addEventListener("click", deleteToDo);
+    li.appendChild(span);
+    li.appendChild(button);
+    toDoList.appendChild(li);
+}
+
+function handleToDoSubmit(event) {
+    event.preventDefault();
+    const newTodo=toDoInput.value;
+    toDoInput.value="";
+    paintToDo(newTodo);
+}
+
+function deleteToDo(event) {
+    const li=event.target.parentElement;
+    li.remove();
+}
+toDoFrom.addEventListener("submit", handleToDoSubmit);
