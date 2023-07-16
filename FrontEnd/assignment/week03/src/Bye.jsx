1330b5c364dbce3b39fa08f3cfe2285633ab59e4
idkwtdoob <rkdwndms333@naver.com>
2023-07-16 18:45:31 +0900

    과제 완료

---
+++ FrontEnd/assignment/week03/src/Bye.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Bye = () => {
+    const name = "쥬은";
+
+    return (
+        <div>
+            바이바이 안녕히 가세요 {name} ㅜ.ㅜ
+        </div>
+    );
+};
+
+export default Bye;
---
+++ FrontEnd/assignment/week03/src/Hello.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Hello = () => {
+    const name = "주은";
+
+    return (
+        <div>하이루 방가방가, {name}!</div>
+    );
+};
+
+export default Hello;
